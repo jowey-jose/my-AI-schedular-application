ce8735ae1a72b217e5ade44beae4d5d237c162a2
jowey-jose <josephmbuku17@gmail.com>
2019-10-09 00:28:45 +0300

    Setup configuration with syncfusion angular schedular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+// import {view} from '@syncfusion-ej2-schedule';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<ejs-schedule height="850" width="1250"></ejs-schedule>',
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class AppComponent {
-  title = 'my-ai-schedular-app';
+  title = 'my-AI-schedular-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScheduleModule, RecurrenceEditorModule, DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScheduleModule, RecurrenceEditorModule
   ],
-  providers: [],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, MonthAgendaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
